564a6cd273f0417e6b9afd0e555b1d4ccaab9a1c
KellyMadland <kellymadland@gmail.com>
2023-11-14 07:39:04 -0600

    bootstrap_practice

---
+++ bootstrap_practice/src/test/java/com/cupitmadland/bootstrap_practice/AppTest.java
@@ -0,0 +1,20 @@
+package com.cupitmadland.bootstrap_practice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
