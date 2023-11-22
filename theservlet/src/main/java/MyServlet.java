e1e4aac4a99c4646b1f7e93708d2e90531e4df56
KellyMadland <kellymadland@gmail.com>
2023-11-22 16:44:04 -0600

    the servlet project

---
+++ theservlet/src/main/java/MyServlet.java
@@ -0,0 +1,65 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+			message = "Good Afternoon, Kelly";
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("\n" + message );
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		PrintWriter pw = response.getWriter();
+		response.setContentType("text/html");
+		
+		pw.print("<html><head>");
+		pw.print("<title> Welcome to the World of Servlets</title>\n");
+		pw.println("<h2> username is:" + username + "</h2>");
+		pw.println("<h2> password is:" + password + "</h2>");
+		pw.print("</head></html>");
+		
+	}
+
+}
